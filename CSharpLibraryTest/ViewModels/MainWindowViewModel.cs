e863c5a46991b3dd431ab29b755b4ac556601991
toshimana <toshi.mana.plus1@gmail.com>
2021-06-20 17:39:00 +0900

    GLの描画を実装

--- CSharpLibraryTest/ViewModels/MainWindowViewModel.cs
+++ CSharpLibraryTest/ViewModels/MainWindowViewModel.cs
@@ -2,6 +2,7 @@
 using Prism.Mvvm;
 using Reactive.Bindings;
 using System;
+using System.IO;
 using System.Windows;
 using System.Windows.Media.Media3D;
 
@@ -12,7 +13,7 @@ namespace CSharpLibraryTest.ViewModels
         public ReactiveProperty<string> Title { get; } = new ReactiveProperty<string>("CSharpLibraryTest");
         public ReactiveProperty<string> ImagePath { get; } = new ReactiveProperty<string>("");
         public ReactiveCommand ImageLoadCommand { get; } = new ReactiveCommand();
-        public ReactiveProperty<string> ModelPath { get; } = new ReactiveProperty<string>("");
+        public ReactiveProperty<string> ModelPath { get; } = new ReactiveProperty<string>(@"C:\Users\toshi\Downloads\Hydrant\Hydrant.stl");
         public ReactiveCommand ModelLoadCommand { get; } = new ReactiveCommand();
 
         public ReactiveProperty<Mat> Image { get; } = new ReactiveProperty<Mat>();
@@ -20,7 +21,7 @@ namespace CSharpLibraryTest.ViewModels
 
         private Func<MessageBoxResult> PathLoadFailDialog = () => MessageBoxResult.OK;
 
-
+        public Model3DGroup TargetModel3DGroup = null;
 
         public MainWindowViewModel()
         {
@@ -48,9 +49,12 @@ namespace CSharpLibraryTest.ViewModels
         
         private void LoadModel()
         {
+            if (ModelPath == null) return;
+            if (!File.Exists(ModelPath.Value)) return;
+
             var reader = new HelixToolkit.Wpf.StLReader();
-            var mgs = reader.Read(ModelPath.Value);
-            foreach (var model in mgs.Children)
+            TargetModel3DGroup = reader.Read(ModelPath.Value);
+            foreach (var model in TargetModel3DGroup.Children)
             {
                 var m = model as GeometryModel3D;
                 var meshs = m.Geometry as MeshGeometry3D;
--- CSharpLibraryTest/Views/MainWindow.xaml.cs
+++ CSharpLibraryTest/Views/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using OpenTK.Wpf;
 using Reactive.Bindings;
 using System;
 using System.Windows;
+using System.Windows.Media.Media3D;
 
 namespace CSharpLibraryTest.Views
 {
@@ -31,23 +32,57 @@ namespace CSharpLibraryTest.Views
 
         private void OpenTkControl_OnRender(TimeSpan delta)
         {
+            var vm = DataContext as MainWindowViewModel;
+            if (vm == null) return;
+            if (vm.TargetModel3DGroup == null) return;
+
             var hue = 0.15f;
             var c = Color4.FromHsv(new Vector4(hue, 0.75f, 0.75f, 1));
             GL.ClearColor(c);
             GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            GL.Viewport(0, 0, (int)OpenTKControl.ActualWidth, (int)OpenTKControl.ActualHeight);
+
+            GL.MatrixMode(MatrixMode.Projection);
             GL.LoadIdentity();
-            GL.Begin(PrimitiveType.Triangles);
+            float aspect = (float)(OpenTKControl.ActualWidth / OpenTKControl.ActualHeight);
+            Matrix4 proj = Matrix4.CreatePerspectiveFieldOfView(1.5708f, aspect, 0.01f, 10000.0f);
+            GL.LoadMatrix(ref proj);
 
-            GL.Color4(Color4.Red);
-            GL.Vertex2(0.0f, 0.5f);
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadIdentity();
 
-            GL.Color4(Color4.Green);
-            GL.Vertex2(0.58f, -0.5f);
+            foreach (var model in vm.TargetModel3DGroup.Children)
+            {
 
-            GL.Color4(Color4.Blue);
-            GL.Vertex2(-0.58f, -0.5f);
+                var m = model as GeometryModel3D;
+                var meshs = m.Geometry as MeshGeometry3D;
 
-            GL.End();
+                Vector3 eye = new Vector3(0.0f, 0.0f, 500.0f);
+                Point3D tp = meshs.Bounds.Location;
+                Vector3 target = new Vector3((float)tp.X, (float)tp.Y, (float)tp.Z);
+                Vector3 up = new Vector3(0.0f, 1.0f, 0.0f);
+                Matrix4 look = Matrix4.LookAt(eye, target, up);
+                GL.LoadMatrix(ref look);
+
+                GL.Begin(PrimitiveType.Points);
+                GL.Color4(Color4.Blue);
+                GL.Vertex3(target);
+                GL.End();
+
+                Point3DCollection point3s = meshs.Positions;
+
+                //int VertexBufferObject = GL.GenBuffer();
+                //GL.BindBuffer(BufferTarget.ArrayBuffer, VertexBufferObject);
+                //GL.BufferData(BufferTarget.ArrayBuffer, point3s.Count, point3s);
+                GL.Begin(PrimitiveType.Points);
+                foreach (Point3D p in point3s)
+                {
+                    GL.Color4(Color4.Red);
+                    GL.Vertex3(p.X, p.Y, p.Z);
+                }
+                GL.End();
+            }
             GL.Finish();
         }
     }

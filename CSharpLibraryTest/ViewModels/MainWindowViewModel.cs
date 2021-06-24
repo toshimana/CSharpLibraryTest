dec46d0082faefdb4d6d01aeccd7c3f9deebabd1
toshimana <toshi.mana.plus1@gmail.com>
2021-06-24 23:25:08 +0900

    OBJファイル出力機能を追加

--- CSharpLibraryTest/ViewModels/MainWindowViewModel.cs
+++ CSharpLibraryTest/ViewModels/MainWindowViewModel.cs
@@ -5,8 +5,6 @@ using Reactive.Bindings;
 using System;
 using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Media.Media3D;
--- CSharpLibraryTest/Views/MainWindow.xaml.cs
+++ CSharpLibraryTest/Views/MainWindow.xaml.cs
@@ -1,12 +1,8 @@
 ﻿using CSharpLibraryTest.ViewModels;
-using Reactive.Bindings.Interactivity;
-using System;
-using System.Reactive.Linq;
+using HelixToolkit.Wpf;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Media3D;
+using System.Windows.Forms;
 
 namespace CSharpLibraryTest.Views
 {
@@ -21,9 +17,27 @@ namespace CSharpLibraryTest.Views
             InitializeComponent();
 
             var vm = DataContext as MainWindowViewModel;
-            vm.Initialize(() => MessageBox.Show("読み込みに失敗しました", "画像読み込み失敗", MessageBoxButton.OK, MessageBoxImage.Warning));
+            vm.Initialize(() => System.Windows.MessageBox.Show("読み込みに失敗しました", "画像読み込み失敗", MessageBoxButton.OK, MessageBoxImage.Warning));
         }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new SaveFileDialog();
 
+            dialog.Filter = "OBJ File|*.obj";
+            if (dialog.ShowDialog()
+                == System.Windows.Forms.DialogResult.OK)
+            {
+                var stream = dialog.OpenFile();
+                var path = dialog.FileName;
+                var parentDir = Directory.GetParent(path);
 
+                var objWriter = new ObjExporter();
+                objWriter.TextureFolder = parentDir.FullName;
+                objWriter.FileCreator = f => File.Create(System.IO.Path.Combine(parentDir.FullName, f));
+                objWriter.MaterialsFile = System.IO.Path.ChangeExtension(Path.GetFileName(path), ".mtl");
+                objWriter.Export(viewport, stream);
+
+            }
+        }
     }
 }

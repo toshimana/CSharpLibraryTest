99b620d22fa051aa9f619d2a7775e1c9f2bc0def
toshimana <toshi.mana.plus1@gmail.com>
2021-06-27 22:07:10 +0900

    テストの追加

---
+++ CSharpLibraryTestUnitTest/MainWindowViewModelTest.cs
@@ -0,0 +1,58 @@
+using CSharpLibraryTest.Services;
+using CSharpLibraryTest.ViewModels;
+using Moq;
+using Prism.Services.Dialogs;
+using System;
+using System.Windows.Media.Media3D;
+using Xunit;
+
+namespace CSharpLibraryTestUnitTest
+{
+    public class MainWindowViewModelTest
+    {
+        private MainWindowViewModel _mw;
+        Mock<IDialogService> _dialogServiceMock;
+        Mock<IMessageService> _messageServiceMock;
+        public MainWindowViewModelTest()
+        {
+            _dialogServiceMock = new Mock<IDialogService>();
+            _messageServiceMock = new Mock<IMessageService>();
+            this._mw = new MainWindowViewModel(_dialogServiceMock.Object, _messageServiceMock.Object);
+        }
+
+        [Fact]
+        public void LoadImageTest()
+        {
+            Assert.Null(_mw.Image.Value);
+
+            var path = @"TestData\Lenna.jpg";
+            _mw.ImagePath.Value = path;
+            _mw.ImageLoadCommand.Execute();
+
+            Assert.NotNull(_mw.Image.Value);
+        }
+
+        [Fact]
+        public void LoadModelTest()
+        {
+            _dialogServiceMock.Setup(x => x.ShowDialog(It.IsAny<string>(), It.IsAny<IDialogParameters>(), It.IsAny<Action<IDialogResult>>()))
+                .Callback<string, IDialogParameters, Action<IDialogResult>>((n, p, c) => c(new DialogResult(ButtonResult.OK)));
+
+            var path = @"TestData\Hydrant.stl";
+
+            var p = new DialogParameters();
+            p.Add(nameof(ViewReadModelViewModel.ReadModel), @"");
+
+            DialogResult receivedResult = null;
+            _dialogServiceMock.Object.ShowDialog(nameof(ViewReadModelViewModel), p, r => receivedResult = r as DialogResult);
+
+
+            Assert.Null(_mw.ModelData.Value);
+
+            _mw.ModelPath.Value = path;
+            _mw.ModelLoadCommand.Execute();
+
+            Assert.Equal(ButtonResult.OK, receivedResult.Result);
+        }
+    }
+}
